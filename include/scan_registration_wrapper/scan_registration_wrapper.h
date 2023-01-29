e43b50b69e7cb0ae66f6fbc15eb2585280a757a6
Refstop <bhbhchoi@gmail.com>
2023-01-29 19:42:38 +0900

    Fix issues

--- include/scan_registration_wrapper/scan_registration_wrapper.h
+++ include/scan_registration_wrapper/scan_registration_wrapper.h
@@ -2,33 +2,30 @@
 // Created by bhbhchoi on 23. 1. 26.
 //
 
-#ifndef SP_A_LOAM_SCAN_REGISTRATION_WRAPPER_H
-#define SP_A_LOAM_SCAN_REGISTRATION_WRAPPER_H
-#include <rclcpp/rclcpp.hpp>
-#include <builtin_interfaces/msg/time.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
+#ifndef SP_A_LOAM_ROS_SCAN_REGISTRATION_WRAPPER_H
+#define SP_A_LOAM_ROS_SCAN_REGISTRATION_WRAPPER_H
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 
 #include "common.h"
 #include "scanRegistration/scanRegistration.h"
 
-//using std::placeholders::_1;
 using namespace std::chrono_literals;
 
-class scanRegistrationWrapper: public rclcpp::Node {
+class scanRegistrationWrapper {
 public:
     scanRegistrationWrapper(scanRegistrationOption option);
 private:
-    void laserCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr laser_cloud_msg);
-    void pclToMsg(const pcl::PointCloud<PointType> &laser_cloud, sensor_msgs::msg::PointCloud2 &laser_cloud_out_msg, const builtin_interfaces::msg::Time timestamp);
+    void laserCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& laser_cloud_msg);
+    void pclToMsg(const pcl::PointCloud<PointType> &laser_cloud, sensor_msgs::PointCloud2 &laser_cloud_out_msg, const ros::Time timestamp);
     std::unique_ptr<scanRegistration> scan_registration_handler_;
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_laser_cloud_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_laser_cloud_,
-                                                                pub_corner_points_sharp_,
-                                                                pub_corner_points_less_sharp_,
-                                                                pub_surf_points_flat_,
-                                                                pub_surf_points_less_flat_;
-
-
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_laser_cloud_;
+    ros::Publisher pub_laser_cloud_,
+                   pub_corner_points_sharp_,
+                   pub_corner_points_less_sharp_,
+                   pub_surf_points_flat_,
+                   pub_surf_points_less_flat_;
 };
 
-#endif //SP_A_LOAM_SCAN_REGISTRATION_WRAPPER_H
+#endif //SP_A_LOAM_ROS_SCAN_REGISTRATION_WRAPPER_H
